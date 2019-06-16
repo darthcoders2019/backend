baabbade9772476356d37c9c1bcde113d5ab4a27
Brendon <brendon@ennov.io>
2019-06-16 10:12:41 +0400

    small change

--- src/app.js
+++ src/app.js
@@ -23,20 +23,26 @@ app.use(bodyParser.json({
     limit: '5mb'
 }));
 
-app.use(bodyParser.urlencoded({
-    limit: '5mb',
-    extended: true
-}));
+
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-id, x-accounting-id");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    /*var err = new Error('Not Found');
+     err.status = 404;
+     next(err);*/
 
-    if (req.method === 'OPTIONS') {
-        res.end();
-    } else
-        next();
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use(mrq.db);
